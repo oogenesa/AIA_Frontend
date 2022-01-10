5b4fcb1bd707d75cd01dc1ceb98a8c66a06c23c2
Alrynto <48040209+oogenesa@users.noreply.github.com>
2022-01-10 15:01:37 +0700

    first commit
    Material UI header and footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./views/Footer";
+import Header from "./views/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
